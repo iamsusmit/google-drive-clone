464a9eb8246e2b4ce7efd9730b9455a34b712db6
susmit <susmitmaji14@gmail.com>
2021-03-23 17:08:30 +0530

    adding splitter and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { lazy, Suspense, useState } from "react";
+import {
+  PaneDirective,
+  PanesDirective,
+  SplitterComponent,
+} from "@syncfusion/ej2-react-layouts";
+import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+import "../node_modules/@syncfusion/ej2-layouts/styles/material.css";
+import { Skeleton } from "antd";
+import "antd/dist/antd.css";
 import "./App.css";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
-import FilesView from "./components/filesView/FilesView";
+// import FilesView from "./components/filesView/FilesView";
 import SideIcons from "./components/sideIcons";
-
 import GDriveLogo from "./media/google-drive-logo.png";
-
 import { auth, provider } from "./firebase";
-import { useState } from "react";
+
+const LazyFileView = lazy(() => import("./components/filesView/FilesView"));
 
 function App() {
   const [user, setUser] = useState();
@@ -24,6 +33,23 @@ function App() {
         });
     }
   };
+  const sidebarPane = () => {
+    return (
+      <div className="app__main">
+        <Sidebar />
+      </div>
+    );
+  };
+
+  const fileViewPane = () => {
+    return (
+      <div className="app__main">
+        <Suspense fallback={<Skeleton active />}>
+          <LazyFileView />
+        </Suspense>
+      </div>
+    );
+  };
 
   return (
     <div className="app">
@@ -31,8 +57,18 @@ function App() {
         <>
           <Header userPhoto={user?.photoURL} />
           <div className="app__main">
-            <Sidebar />
-            <FilesView />
+            <SplitterComponent id="separator" separatorSize={5}>
+              <PanesDirective>
+                <PaneDirective
+                  content={sidebarPane}
+                  min="10%"
+                  max="20%"
+                  size="15%"
+                />
+                <PaneDirective content={fileViewPane} min="80%" max="90%" />
+              </PanesDirective>
+            </SplitterComponent>
+
             <SideIcons />
           </div>
         </>
